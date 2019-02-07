8bbc69ee9f4d1019dae96546cca50b4009dd1965
Bhavik Gosrani <bhavik4748@gmail.com>
2019-02-07 07:29:20 -0500

    added cancel booking method

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -173,5 +173,22 @@ module.exports = {
             updatedAt: new Date(result._doc.updatedAt).toISOString(),
         };
 
+    },
+
+    cancelBooking: async args => {
+        try {
+            const booking = await Booking.findById(args.bookingId).populate('event');
+           // if (booking == null)
+           //     throw err;
+            const event = {
+                ...booking.event._doc,
+                _id: booking.event.id,
+                creator: user.bind(this, booking.event._doc.creator)
+            };
+            await Booking.deleteOne({ _id: args.bookingId });
+            return event;
+        } catch (err) {
+            throw err;
+        }
     }
 };
